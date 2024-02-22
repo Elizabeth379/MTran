8ae8c437c9621885277e5d4054477353697ddd5d
Elizabeth <bellator.379@gmail.com>
2024-02-23 02:44:22 +0300

    Lab 2 in process: keywords, libraries, operators, data types and exc. done.

---
+++ LW2/lab2/input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include constants.h
+class Rectangle {
+private:
+    int width;
+    int height;
+public:
+    Rectangle(int w, int h){}
+    void setWidth(int w) { width= w; }
+    void setHeight(int h) { height=h; }
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    int area() const { return width * height; } // comments
+};
+int main() {
+    Rectangle rect(5, 3);
+    std::cout << "Width: " << rect.getWidth() << std::endl;
+    std::cout << "Height: " << rect.getHeight() << std::endl;
+    std::cout << "Area of the rectangle: " << rect.area() << std::endl;
+    rect.setWidth(7);
+    rect.setHeight(4);
+    std::cout << "Updated area of the rectangle: " << rect.area() << std::endl;
+    return 0;
+}
---
+++ LW2/lab2/main.py
@@ -0,0 +1,62 @@
+import re
+
+tokens = [
+    ('CLASS', r'(?<=\bclass\s)([a-zA-Z_]\w*)\b'),
+    ('KEYWORD', r'\b(if|else|while|for|return|class|struct|using|namespace|system|break|continue|do|switch|case|'
+                r'default|std::swap|private|public|protected|const|override|virtual|typename|typedef|template|'
+                r'delete|malloc|calloc|realloc|sizeof|NULL|printf|new|static|operator|this|friend|auto|nullptr|'
+                r'try|catch|exception|setlocale)\b'),
+    ('PREPROCESSOR_DIRECTIVE', r'#include'),
+    ('STANDARD_LIBRARY', r'<\w+>'),
+    ('COMMA', r','),
+    ('END_OF_CONSTRUCTION', r';'),
+    ('OUTPUT', r'cout'),
+    ('NAMESPACE', r'std'),
+    ('OUTPUT_MANIPULATOR', r'endl'),
+    ('MACRO', r'#\w+'),
+    ('COMMENT', r'//.*|/\*.*?\*/'),
+    ('PARENTHESIS', r'[\(\)\{\}\[\]]'),
+    ('ARROW_POINTER', r'->'),
+    ('COLON', r':'),
+    ('DOUBLE_COLON', r'::'),
+    ('BITWISE_OPERATOR', r'[\^|]'),
+    ('OPERATOR', r'([.*<>&~]|>>|<<)'),
+    ('LOGICAL_OPERATOR', r'(&&|\|\||!)'),
+    ('COMPARISON_OPERATOR', r'(==|!=|>=|<=)'),
+    ('ARITHMETIC_OPERATOR', r'(\+\+|\-\-|\+=|-=|\*=|/=|%=|[-+/=%])'),
+    ('TERNARY_OPERATOR', r'\?'),
+    ('ASSIGMENT_OPERATOR', r'='),
+    ('DATA_TYPE', r'\b(bool|signed char|short|long|long long|int|float|double|char|void|unsigned char|unsigned short|'
+                  r'unsigned int|unsigned long|unsigned long long|long double|wchar_t|char8_t|char16_t|char32_t|string|'
+                  r'uint32_t|size_t)\b'),
+    ('STREAM_CLASS', r'\b(ifstream|ofstream|ostream|iostream)\b'),
+    ('STANDARD_FUNCTION', r'\b(strcpy|strlen|strcat|strstr|strtok|sprintf|strcmp|strchr|strrchr)\b'),
+    ('FUNCTION', r'\b(pow|sqrt|sin|cos|log|exp|round)\b'),
+    ('CONTAINER', r'\b(vector|list|map|unordered_map|multimap|deque|queue|stack|set|unordered_set|multiset|bitset)\b'),
+    ('SPECIAL_SYMBOL', r'[#$№@]'),
+    ('HEADER_FILE', r'\b[a-zA-Z_]\w*\.h\b')
+]
+
+
+def tokenize(code):
+    token_regex = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
+    for match in re.finditer(token_regex, code):
+        for name, value in match.groupdict().items():
+            if value is not None:
+                yield name, value
+
+
+def write_to_output(tokens):
+    with open("output.txt", "w") as output_file:
+        output_file.write("Number\t\tElement\t\tCategory\n")
+        for idx, (token_type, value) in enumerate(tokens, 1):
+            output_file.write(f"{idx}\t\t\t{value}\t\t\t{token_type}\n")
+
+
+if __name__ == "__main__":
+    with open("input.cpp", "r") as input_file:
+        code = input_file.read()
+
+    tokens = list(tokenize(code))
+
+    write_to_output(tokens)
